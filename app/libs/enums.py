e67a6331de00946ab52200f1bf68787bd980a7dc
tianyuan233 <1406282384@qq.com>
2018-09-23 17:55:36 +0800

    实现通过POST方式注册client

--- app/libs/enums.py
+++ app/libs/enums.py
@@ -1,11 +1,8 @@
 from enum import Enum
 
-
+#//TODO 枚举类的用法整理
 class ClientTypeEnum(Enum):
     USER_EMAIL = 100
     USER_MOBILE = 101
-
-    # 微信小程序
     USER_MINA = 200
-    # 微信公众号
     USER_WX = 201
