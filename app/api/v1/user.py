5a90e0c2dfd8c0b4c62df0ba72f635246cb5bf24
tianyuan233 <1406282384@qq.com>
2018-09-27 23:17:41 +0800

    DELETE删除和GET获取用户信息

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -8,21 +8,25 @@ from app.models.user import User
 
 api = Redprint('user')
 
-#用户GET获取自己的用户信息
+
+# 用户GET获取自己的用户信息
 @api.route('', methods=['GET'])
 @auth.login_required
 def get_user():
     uid = g.user.uid
     user = User.query.filter_by(id=uid).first_or_404()
     return jsonify(user)
-#管理员获取任意用户的信息
+
+
+# 管理员获取任意用户的信息
 @api.route('/<int:uid>', methods=['GET'])
 @auth.login_required
 def super_get_user(uid):
     user = User.query.filter_by(id=uid).first_or_404()
     return jsonify(user)
 
-#用户DELETE ‘删除’自己的用户信息
+
+# 用户DELETE ‘删除’自己的用户信息
 @api.route('', methods=['DELETE'])
 @auth.login_required
 def delete_user(uid):
@@ -32,7 +36,8 @@ def delete_user(uid):
         user.delete()
     return DeleteSuccess()
 
-#管理员‘删除’任意用户的信息
+
+# 管理员‘删除’任意用户的信息
 @api.route('/<int:uid>', methods=['DELETE'])
 @auth.login_required
 def super_delete_user(uid):
